d38ae4ec1894b936d1e9e083d08380dac7cec020
47876971X <47876971X@A36O14.novaulas.stucom.com>
2017-03-14 12:25:27 +0100

    Noves dades

---
+++ index.php
@@ -0,0 +1,11 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <a href="NewUser.php">Registrarse</a><br>
+        
+    </body>
+</html>
